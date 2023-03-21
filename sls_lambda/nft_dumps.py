0a6aa6e37af0c3f35d7dec0a693ddc0c36d83b33
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-03-21 17:29:08 +0100

    refac: refactored nftdumpsrunner

--- sls_lambda/nft_dumps.py
+++ sls_lambda/nft_dumps.py
@@ -15,28 +15,24 @@ class NFTokenDump(BaseLambdaRunner):
         self.base_url = "https://api.xrpldata.com/api/v1/xls20-nfts/issuer"
         self._set_writer("nft")
 
-    def _get_output_path(self, issuer: str) -> str:
-        now = datetime.datetime.utcnow()
-        if self.environment == "LOCAL":
-            return f"{now.strftime('%Y-%m-%d-%H')}/{issuer}.json"
-        return f"{now.strftime('%Y-%m-%d-%H')}/{issuer}.json"
-
     async def _fetch_nfts(self, issuer):
+        issuers_df = self.factory.issuers_df
         async with aiohttp.ClientSession() as session:
             async with session.get(
                 f"{self.base_url}/{issuer}"
             ) as response:
                 content = await response.content.read()
                 data = json.loads(content)["data"]
-                taxon = self.issuers_df[self.issuers_df["Issuer_Account"] == issuer].Taxon.to_list()
+                taxon = issuers_df[issuers_df["Issuer_Account"] == issuer].Taxon.to_list()
                 if str(taxon[0]) != "nan":
                     target_nfts = [nft for nft in data["nfts"] if nft["Taxon"] == taxon[0]]
                     data["nfts"] = target_nfts
                 return data
 
     async def _dump_issuer_nfts(self, issuer: str):
+        now = datetime.datetime.utcnow()
         data = await self._fetch_nfts(issuer)
-        path = self._get_output_path(issuer)
+        path = f"{now.strftime('%Y-%m-%d-%H')}/{issuer}.json"
         await self.writer.write_json(path, data)
         return {
             "issuer": issuer,
@@ -51,8 +47,9 @@ class NFTokenDump(BaseLambdaRunner):
         }
 
     async def run(self):
+        supported_issuers = self.factory.supported_issuers
         supply = []
-        for chunk in chunks(self.supported_issuers, 10):
+        for chunk in chunks(supported_issuers, 10):
             result = await asyncio.gather(*[self._dump_issuer_nfts(issuer) for issuer in chunk])
             supply.extend(result)
             time.sleep(60)
@@ -73,18 +70,13 @@ class NFTaxonDump(BaseLambdaRunner):
                 data = json.loads(content)
                 return data["data"]
 
-    def _get_output_path(self) -> str:
-        if self.environment == "LOCAL":
-            return "taxon.json"
-        return "taxon.json"
-
     async def run(self):
+        supported_issuers = self.factory.supported_issuers
         taxons = []
-        for chunk in chunks(self.supported_issuers, 10):
+        for chunk in chunks(supported_issuers, 10):
             chunk_taxons = await asyncio.gather(
                 *[self._get_issuer_taxons(issuer) for issuer in chunk]
             )
             taxons.extend(chunk_taxons)
             time.sleep(60)
-        path = self._get_output_path()
-        await self.writer.write_json(path, taxons)
+        await self.writer.write_json("taxon.json", taxons)
