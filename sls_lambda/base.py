bef8b248d9903616b06df51ad69d09af7fbff83e
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-03-21 17:28:39 +0100

    refac: refactored baselambdarunner

--- sls_lambda/base.py
+++ sls_lambda/base.py
@@ -1,14 +1,11 @@
 from abc import ABCMeta, abstractmethod
+
 from utilities import Factory
 
 class BaseLambdaRunner(metaclass=ABCMeta):
     def __init__(self, factory: Factory):
         self.factory = factory
         self.db_client = factory.get_db_client()
-        self.issuers_df = factory.issuers_df
-        self.supported_issuers = factory.supported_issuers
-        self.environment = factory.config.ENVIRONMENT
-        self.config = factory.config
 
     def _set_writer(self, section):
         self.writer = self.factory.get_writer(section)
