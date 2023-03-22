2aa5af12c66e1490009113a2d219a25b7bd44dce
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-03-22 02:03:41 +0100

    feat: added token dump runner to main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import asyncio
 import logging
-from sls_lambda import NFTokenPriceDump, IssuerPriceDump
+from sls_lambda import NFTokenPriceDump, NFTokenDump
 from utilities import factory
 
 
@@ -14,7 +15,8 @@ logger.setLevel(logging.INFO)
 
 if __name__ == "__main__":
     token_price_runner = NFTokenPriceDump(factory)
+    tokens_dump_runner = NFTokenDump(factory)
     # issuer_price_runner = IssuerPriceDump(factory)
-
+    asyncio.run(tokens_dump_runner.run())
     token_price_runner.run()
     # issuer_price_runner.run()
