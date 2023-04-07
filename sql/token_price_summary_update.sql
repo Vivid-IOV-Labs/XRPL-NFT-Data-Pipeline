e9f60aba7965a09669c1d75d461bede38a6ee59e
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-04-07 12:21:42 +0100

    fix: fixed pricing update sql

--- sql/token_price_summary_update.sql
+++ sql/token_price_summary_update.sql
@@ -6,5 +6,5 @@ SELECT nft_token_id,
        MAX(CASE WHEN is_sell_offer = FALSE THEN amount END) AS max_buy_offer
 FROM nft_buy_sell_offers
 WHERE currency = ''
-GROUP BY nft_token_id
+GROUP BY nft_token_id, issuer, taxon
 ON CONFLICT(nft_token_id) DO UPDATE SET floor_price = EXCLUDED.floor_price, max_buy_offer = EXCLUDED.max_buy_offer;
