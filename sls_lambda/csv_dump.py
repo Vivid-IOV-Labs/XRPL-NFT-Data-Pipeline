7dd4fdc405ba5654fcd6d4b310371abbcd6b8d8d
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-05-22 11:50:39 +0100

    refac: refactored csv dumps

--- sls_lambda/csv_dump.py
+++ sls_lambda/csv_dump.py
@@ -1,5 +1,6 @@
 import asyncio
 import datetime
+from typing import List, Tuple
 
 import pandas as pd
 
@@ -13,19 +14,19 @@ class CSVDump(BaseLambdaRunner):
         super().__init__(factory)
         self._set_writer("csv")
 
-    async def run(self) -> None:
-        issuers = self.factory.supported_issuers
-        last_hour = datetime.datetime.utcnow().strftime("%Y-%m-%d-%H")
+    async def _get_supply_volume_df(self, last_hour: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
         supply, volume = await asyncio.gather(*[read_json(
             self.factory.config.NFT_DUMP_BUCKET, "supply.json", self.factory.config
         ), read_json(
             self.factory.config.TAXON_PRICE_DUMP_BUCKET,
-                f"volume/{last_hour}.json",
-                self.factory.config,
+            f"volume/{last_hour}.json",
+            self.factory.config,
         )])
         supply_df = pd.DataFrame(supply)
         volume_df = pd.DataFrame(volume)
-        issuers_df = self.factory.issuers_df
+        return supply_df, volume_df
+
+    async def _read_price_files_from_s3(self, issuers: List, last_hour: str):
         prices = await asyncio.gather(
             *[
                 read_json(
@@ -36,6 +37,27 @@ class CSVDump(BaseLambdaRunner):
                 for issuer in issuers
             ]
         )
+        return prices
+
+    def _merge_dfs(  # noqa
+            self,
+            price_df: pd.DataFrame,
+            supply_df: pd.DataFrame,
+            volume_df: pd.DataFrame,
+            issuers_df: pd.DataFrame
+        ) -> pd.DataFrame:
+        supply_volume_merge = supply_df.merge(volume_df, how="left", on=["ISSUER"])
+        supply_volume_merge = supply_volume_merge.fillna(0)
+        merged_1 = price_df.merge(supply_volume_merge, how="inner", on=["ISSUER"])
+        final_merge = merged_1.merge(issuers_df, how="inner", on=["ISSUER"])
+        return final_merge
+
+    async def _run(self):
+        issuers = self.factory.supported_issuers
+        last_hour = datetime.datetime.utcnow().strftime("%Y-%m-%d-%H")
+        supply_df, volume_df = await self._get_supply_volume_df(last_hour)
+        issuers_df = self.factory.issuers_df
+        prices = await self._read_price_files_from_s3(issuers, last_hour)
         price_df = pd.DataFrame([price for price in prices if price is not None])
         supply_df.rename(
             columns={
@@ -74,10 +96,7 @@ class CSVDump(BaseLambdaRunner):
         )
         price_df["PRICEXRP"] = price_df["MID_PRICE_XRP"]
 
-        supply_volume_merge = supply_df.merge(volume_df, how="left", on=["ISSUER"])
-        supply_volume_merge = supply_volume_merge.fillna(0)
-        merged_1 = price_df.merge(supply_volume_merge, how="inner", on=["ISSUER"])
-        final_merge = merged_1.merge(issuers_df, how="inner", on=["ISSUER"])
+        final_merge = self._merge_dfs(price_df, supply_df, volume_df, issuers_df)
         final_df = final_merge[
             [
                 "ISSUER",
@@ -104,3 +123,6 @@ class CSVDump(BaseLambdaRunner):
                 self.writer.write_df(price_df, f"{last_hour}_price.csv", "csv"),
             ]
         )
+
+    def run(self) -> None:
+        asyncio.run(self._run())
