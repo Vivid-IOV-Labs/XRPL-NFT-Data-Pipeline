16007d6b248de2c7e562e6740e51e07c08eda989
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-06-15 10:52:54 +0100

    done

--- sls_lambda/__init__.py
+++ sls_lambda/__init__.py
@@ -1,6 +1,6 @@
 from .csv_dump import CSVDump
 from .graph import GraphDumps, TaxonPriceGraph
 from .nft_dumps import NFTaxonDump, NFTokenDump
-from .pricing import IssuerPriceDump, NFTokenPriceDump, TaxonPriceDump
+from .pricing import IssuerPriceDump, TaxonPriceDump
 from .table import TableDump
 from .twitter import TwitterDump
