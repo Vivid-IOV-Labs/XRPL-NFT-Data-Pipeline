91980ddde41474d4a2cb7278f781935345b01bfd
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-07-12 22:07:48 +0100

    modified the pipeline classes for the config refactoring

--- sls_lambda/handlers.py
+++ sls_lambda/handlers.py
@@ -3,7 +3,7 @@ import logging
 
 from sls_lambda import (CSVDump, GraphDumps, IssuerPriceDump, NFTaxonDump, NFTokenDump, TableDump)
 from sls_lambda.invokers import (invoke_graph_dump, invoke_table_dump)
-from utilities import factory
+from utilities import Factory, Config
 
 logger = logging.getLogger("app_log")
 formatter = logging.Formatter(
@@ -16,16 +16,22 @@ logger.setLevel(logging.INFO)
 
 
 def issuers_nft_dumps(event, context):
+    config = Config.from_env()
+    factory = Factory(config)
     runner = NFTokenDump(factory)
     asyncio.run(runner.run())
 
 
 def issuers_taxon_dumps(event, context):
+    config = Config.from_env()
+    factory = Factory(config)
     runner = NFTaxonDump(factory)
     asyncio.run(runner.run())
 
 
 def issuers_price_dump(event, context):
+    config = Config.from_env()
+    factory = Factory(config)
     issuer_price_runner = IssuerPriceDump(factory)
     asyncio.run(issuer_price_runner.run())
 
@@ -34,22 +40,23 @@ async def after_csv_dump_invocation(config):
     await asyncio.gather(*[invoke_table_dump(config), invoke_graph_dump(config)])
 
 def csv_dump(event, context):
+    config = Config.from_env()
+    factory = Factory(config)
     runner = CSVDump(factory)
     runner.run()
 
-    asyncio.run(after_csv_dump_invocation(factory.config))
+    asyncio.run(after_csv_dump_invocation(config))
 
 def table_dump(event, context):
+    config = Config.from_env()
+    factory = Factory(config)
     runner = TableDump(factory)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(runner.run())
 
 
 def graph_dump(event, context):
+    config = Config.from_env()
+    factory = Factory(config)
     runner = GraphDumps(factory)
     runner.run()
-
-
-# def twitter_dump(event, context):
-#     runner = TwitterDump(factory)
-#     runner.run()
--- sls_lambda/invokers.py
+++ sls_lambda/invokers.py
@@ -3,11 +3,12 @@ import logging
 import aioboto3
 import boto3
 import botocore
+from utilities import Config
 
 logger = logging.getLogger("app_log")
 
 
-def invoke_csv_dump(config):
+def invoke_csv_dump(config: Config):
     lambda_client = boto3.client(
         "lambda",
         region_name="eu-west-2",
@@ -21,8 +22,7 @@ def invoke_csv_dump(config):
     logger.info(resp)
 
 
-async def invoke_token_dumps(config):
-    logger.info("Invoking Token Dumps")
+async def invoke_token_dumps(config: Config):
     session = aioboto3.Session(  # noqa
         aws_access_key_id=config.ACCESS_KEY_ID,
         aws_secret_access_key=config.SECRET_ACCESS_KEY,
@@ -36,7 +36,7 @@ async def invoke_token_dumps(config):
         )
 
 
-async def invoke_taxon_dumps(config):
+async def invoke_taxon_dumps(config: Config):
     logger.info("Invoking Taxon Dumps")
     session = aioboto3.Session(  # noqa
         aws_access_key_id=config.ACCESS_KEY_ID,
@@ -51,7 +51,7 @@ async def invoke_taxon_dumps(config):
         )
 
 
-async def invoke_table_dump(config):
+async def invoke_table_dump(config: Config):
     session = aioboto3.Session(  # noqa
         aws_access_key_id=config.ACCESS_KEY_ID,
         aws_secret_access_key=config.SECRET_ACCESS_KEY,
@@ -65,7 +65,7 @@ async def invoke_table_dump(config):
         )
 
 
-async def invoke_graph_dump(config):
+async def invoke_graph_dump(config: Config):
     session = aioboto3.Session(  # noqa
         aws_access_key_id=config.ACCESS_KEY_ID,
         aws_secret_access_key=config.SECRET_ACCESS_KEY,
@@ -79,7 +79,7 @@ async def invoke_graph_dump(config):
         )
 
 
-async def invoke_twitter_dump(config):
+async def invoke_twitter_dump(config: Config):
     session = aioboto3.Session(  # noqa
         aws_access_key_id=config.ACCESS_KEY_ID,
         aws_secret_access_key=config.SECRET_ACCESS_KEY,
@@ -91,47 +91,3 @@ async def invoke_twitter_dump(config):
             FunctionName=f"nft-twitter-dump-{config.STAGE}",
             InvocationType="Event",
         )
-
-
-# async def invoke_token_pricing_dump(issuer):
-#     session = aioboto3.Session(  # noqa
-#         aws_access_key_id=Config.ACCESS_KEY_ID,
-#         aws_secret_access_key=Config.SECRET_ACCESS_KEY,
-#         region_name="eu-west-2",
-#     )
-#     config = botocore.config.Config(read_timeout=900)
-#     async with session.client("lambda", config=config) as lambda_client:
-#         await lambda_client.invoke(
-#             FunctionName=f"issuer-token-price-dumps-{Config.STAGE}",
-#             InvocationType="RequestResponse",
-#             Payload=json.dumps({"issuer": issuer}).encode("utf-8"),
-#         )
-#
-#
-# async def invoke_issuer_price_dump(issuer):
-#     session = aioboto3.Session(  # noqa
-#         aws_access_key_id=Config.ACCESS_KEY_ID,
-#         aws_secret_access_key=Config.SECRET_ACCESS_KEY,
-#         region_name="eu-west-2",
-#     )
-#     config = botocore.config.Config(read_timeout=900)
-#     async with session.client("lambda", config=config) as lambda_client:
-#         await lambda_client.invoke(
-#             FunctionName=f"issuer-price-dump-{Config.STAGE}",
-#             InvocationType="RequestResponse",
-#             Payload=json.dumps({"issuer": issuer}).encode("utf-8"),
-#         )
-#
-#
-# def invoke_issuers_pricing_dump():
-#     lambda_client = boto3.client(
-#         "lambda",
-#         region_name="eu-west-2",
-#         aws_access_key_id=Config.ACCESS_KEY_ID,
-#         aws_secret_access_key=Config.SECRET_ACCESS_KEY,
-#     )
-#     resp = lambda_client.invoke(
-#         FunctionName=f"tracked-issuers-price-dumps-{Config.STAGE}",
-#         InvocationType="Event",
-#     )
-#     logger.info(resp)
