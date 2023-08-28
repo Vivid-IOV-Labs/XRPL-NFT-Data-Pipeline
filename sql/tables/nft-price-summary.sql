7223a5fb88f4d5966a7e5ce0a3df637c8e4960b8
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-08-28 11:25:38 +0100

    changed max_buy_offer and floor_price column of price summary to double precision

--- sql/tables/nft-price-summary.sql
+++ sql/tables/nft-price-summary.sql
@@ -2,8 +2,8 @@
 CREATE TABLE IF NOT EXISTS nft_pricing_summary
 (
 	nft_token_id text COLLATE pg_catalog."default" UNIQUE,
-	floor_price text COLLATE pg_catalog."default",
-	max_buy_offer text COLLATE pg_catalog."default",
+	floor_price double precision COLLATE pg_catalog."default",
+	max_buy_offer double precision COLLATE pg_catalog."default",
 	issuer text COLLATE pg_catalog."default",
 	taxon bigint,
     burn_offer_hash VARCHAR(255) UNIQUE
