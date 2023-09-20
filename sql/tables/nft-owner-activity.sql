7faa6597bd04bd7fb88976d2ff1d81b8b778a700
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-09-20 10:03:55 +0100

    added unique constraint to the id field of nft-owner-actvity table

--- sql/tables/nft-owner-activity.sql
+++ sql/tables/nft-owner-activity.sql
@@ -1,7 +1,7 @@
 CREATE TYPE ActionType AS ENUM ('Sold', 'Acquired');
 
 CREATE TABLE nft_owner_activity (
-    id UUID DEFAULT uuid_generate_v4(),
+    id UUID DEFAULT uuid_generate_v4() UNIQUE,
     nft_token_id TEXT NOT NULL,
     owner_address TEXT NOT NULL,
     action ActionType NOT NULL,
