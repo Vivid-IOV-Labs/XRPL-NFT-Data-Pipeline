17f704c64c5b7e9187bd8e1c13f3cd9d495d04c9
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2023-09-20 09:53:50 +0100

    added owner activity table sql

---
+++ sql/tables/nft-owner-activity.sql
@@ -0,0 +1,11 @@
+CREATE TYPE ActionType AS ENUM ('Sold', 'Acquired');
+
+CREATE TABLE nft_owner_activity (
+    id UUID DEFAULT uuid_generate_v4(),
+    nft_token_id TEXT NOT NULL,
+    owner_address TEXT NOT NULL,
+    action ActionType NOT NULL,
+    timestamp TIMESTAMP NOT NULL,
+    create_offer_tx_hash TEXT,
+    accept_offer_tx_hash TEXT
+);
